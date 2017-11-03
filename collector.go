1690dc3b471431812466611be37f6eafde50a936
Tim Schindler <xh3b4sd@gmail.com>
2017-11-03 09:53:01 +0100

    implemented basic collector functionality

--- collector.go
+++ collector.go
@@ -1,10 +1,14 @@
 package versionbundle
 
 import (
+	"context"
+	"encoding/json"
 	"net/url"
+	"sync"
 
 	"github.com/giantswarm/microerror"
 	"github.com/go-resty/resty"
+	"golang.org/x/sync/errgroup"
 )
 
 type CollectorConfig struct {
@@ -25,6 +29,7 @@ type Collector struct {
 	restClient *resty.Client
 
 	bundles []Bundle
+	mutex   sync.Mutex
 
 	endpoints []url.URL
 }
@@ -42,6 +47,7 @@ func NewCollector(config CollectorConfig) (*Collector, error) {
 		restClient: config.RestClient,
 
 		bundles: nil,
+		mutex:   sync.Mutex{},
 
 		endpoints: config.Endpoints,
 	}
@@ -50,10 +56,59 @@ func NewCollector(config CollectorConfig) (*Collector, error) {
 }
 
 func (c *Collector) Bundles() []Bundle {
-	return c.bundles
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	return CopyBundles(c.bundles)
 }
 
-// TODO
-func (c *Collector) Collect() error {
+type CollectorEndpointResponse struct {
+	VersionBundles []Bundle `json:"version_bundles"`
+}
+
+func (c *Collector) Collect(ctx context.Context) error {
+	var responses [][]byte
+	{
+		g, ctx := errgroup.WithContext(ctx)
+		responses = make([][]byte, len(c.endpoints))
+
+		for i, e := range c.endpoints {
+			i, e := i, e
+
+			g.Go(func() error {
+				res, err := c.restClient.NewRequest().SetContext(ctx).Get(e.String())
+				if err != nil {
+					return microerror.Mask(err)
+				}
+
+				responses[i] = res.Body()
+
+				return nil
+			})
+		}
+	}
+
+	var bundles []Bundle
+	{
+
+		for _, b := range responses {
+			var er CollectorEndpointResponse
+			err := json.Unmarshal(b, &er)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			for _, bundle := range er.VersionBundles {
+				bundles = append(bundles, bundle)
+			}
+		}
+	}
+
+	{
+		c.mutex.Lock()
+		c.bundles = bundles
+		c.mutex.Unlock()
+	}
+
 	return nil
 }
