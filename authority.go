38cae73a69f4776072f6c7e66df927fd220eb369
Tuomas Makinen <tuommaki@gmail.com>
2018-05-21 13:19:28 +0300

    Add Provider field to Authority

--- authority.go
+++ authority.go
@@ -7,5 +7,6 @@ import (
 type Authority struct {
 	Endpoint *url.URL `yaml:"endpoint"`
 	Name     string   `yaml:"Name"`
+	Provider string   `yaml:"Provider"`
 	Version  string   `yaml:"version"`
 }
