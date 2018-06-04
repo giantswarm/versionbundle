db181bf877a098564cf613e0185ca8aa68ac3def
Federico Gimenez <federico.gimenez@gmail.com>
2018-06-04 15:50:49 +0200

    address review comments

--- index_releases_sort.go
+++ index_releases_sort.go
@@ -4,12 +4,12 @@ import (
 	"github.com/coreos/go-semver/semver"
 )
 
-type SortIndexReleasesByVersion []Release
+type SortIndexReleasesByVersion []IndexRelease
 
 func (r SortIndexReleasesByVersion) Len() int      { return len(r) }
 func (r SortIndexReleasesByVersion) Swap(i, j int) { r[i], r[j] = r[j], r[i] }
 func (r SortIndexReleasesByVersion) Less(i, j int) bool {
-	verA := semver.New(r[i].Version())
-	verB := semver.New(r[j].Version())
+	verA := semver.New(r[i].Version)
+	verB := semver.New(r[j].Version)
 	return verA.LessThan(*verB)
 }
